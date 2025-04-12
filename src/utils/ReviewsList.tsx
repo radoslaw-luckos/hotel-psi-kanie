43e87e505e70f8fc5da21edcc0f36f42f7b37ff7
Radosław Łuckoś <luckos.r@hotmail.com>
2025-04-12 20:25:01 +0200

    minor design changes + new reviews

--- src/utils/ReviewsList.tsx
+++ src/utils/ReviewsList.tsx
@@ -8,7 +8,7 @@ export const REVIEWS = [
         text: 'Obawiałam się głównie lęku separacyjnego u Meli. Mela jest pieskiem lękowym.Na sytuacje stresujące reaguje szczekaniem i naprężeniem całego ciała. Bardzo bałam się, że zostanie odebrana jako "agresywny" pies. Bałam sie też, że zwyczajnie zabraknie Wam cierpliwości. Mele zmęczyć jest trudno, wyciszona bywa tylko w domu 🙂 Przekonało mnie Twoje podejście do zwierząt. Po naszej adaptacyjnej wizycie byłam przekonana, że Mela będzie w odpowiednich rękach. Dyscyplina i miłość to to, czego moja sunia potrzebuje.Podobało mi się dostosowanie do Meli potrzeb - długie spacery, mizianko, poświęcona uwaga. Jak widziałam Wasze zdjęcia serce mi rosło! Widziałam, że Melcia w pełni Cie zaakceptowała.Gdybym miała jeszcze raz decydować, chętnie pozostawiłabym Melanię pod Waszą opieką. Chociaż nie jestem w 100% przekonana czy Mela nadaje sie do takiej formy opieki. Beze mnie zostawała tylko u nas w domu, w bezpiecznych kątach. Myślę, że każdy kolejny raz byłby łatwiejszy dla nas wszystkich 🙂'
     },
     {
-      photo: <StaticImage className='photo' src='../images/Katarzyna_Domanska.png' alt='Zdjęcie Katarzyny Domańskiej'/>,
+      photo: <StaticImage className='photo' src='../images/avatar.png' alt='Avatar opiniującego bez zdjęcia'/>,
       name: 'Katarzyna Domańska',
       text: 'Psi hotel Kanie, to miejsce gdzie psy mogą poczuć domową atmosferę, czułość, troskę i wygodę. Dom - hotelik ma duży ogród, położony jest w pięknej, zielonej okolicy. Psy mieszkają w pokojach, niektóre nawet śpią z gospodarzami:-) Dziękujemy za gościnę!'
     }, 
@@ -28,8 +28,22 @@ export const REVIEWS = [
       text: "Psi hotel Kanie to miejsce magiczne,wyjątkowe.Pracują w nim ludzie tak niesamowicie oddani zwierzętom,rozumiejący ich potrzeby i przepełnieni milością do nich.Moje sunie przebywały w hoteliku 10 dni, były poraz pierwszy beze mnie i oczywiście bałam się jak to zniosą.Okazało się,że super.Pan Bartek przesyłał mi codziennie zdjęcia co było bardzo miłe i uspokajające.Zwierzęta w hoteliku czują się jak u siebie,są zaopiekowane i kochane. Dziękuję panu Bartkowi i Sylwii za opiekę nad Tildą i Helą. Polecam z całego serca to miejsce,ja na pewno będę korzystać."
     },
     {
+      photo: <StaticImage className='photo' src='../images/avatar.png' alt='Avatar opiniującego bez zdjęcia'/>,
+      name: 'Dorota Niedzialek',
+      text: "Najlepsze miejsce dla mojego czworonoga, Ramzes (buldog francuski) nie chce wracać już do domu! Dobra opieka, super kontakt z właścicielem, świetna domowa atmosfera.Codziennie relacje wstawiane przez właścicieli Psiego Hotelu Kanie sprawiają, że można obserwować pupila na bieżąco.Na pewno skorzystamy jeszcze nie raz! Bardzo, bardzo polecam !!!"
+    },
+    {
       photo: <StaticImage className='photo' src='../images/Malgorzata_Palys.png' alt='Zdjęcie Małgorzaty Pałys'/>,
       name: 'Małgorzata Pałys',
       text: 'Moja sunia była w Hoteliku w Kaniach dwukrotnie. Obawiałam się u niej głównie lęku separacyjnego. Mela jest pieskiem lękowym. Na sytuacje stresujące reaguje szczekaniem i naprężeniem całego ciała. Bardzo bałam się, że zostanie odebrana jako "agresywny" pies. Bałam się też, że zwyczajnie zabraknie Wam cierpliwości.Mele zmęczyć jest trudno, wyciszona bywa tylko w domu 🙂 Przekonało mnie Wasze podejście do zwierząt. Po naszej adaptacyjnej wizycie byłam przekonana, że Mela będzie w odpowiednich rękach. Dyscyplina i miłość to to, czego moja sunia potrzebuje. Podobało mi się dostosowanie do Meli potrzeb - długie spacery, mizianko, poświęcona uwaga. Jak widziałam Wasze zdjęcia serce mi rosło! Widziałam, że Melcia w pełni zaakceptowała swoich nowych opiekunów. Gdy oglądam posty Hoteliku i Melcia usłyszy głos Bartka, szuka go po całym mieszkaniu 😊 Wiem, że Melcia wróci do Hoteliku jeszcze nie raz - w końcu mamy miejsce, gdzie ze spokojem i pełnym zaufaniem mogę ją zostawić ❤'
     },
+    {
+      photo: <StaticImage className='photo' src='../images/avatar.png' alt='Avatar opiniującego bez zdjęcia'/>,
+      name: 'Damian Praca',
+      text: 'Pierwszy raz musiałem pozostawić mojego pupila w Hotelu dla psów. Ciesze sie, że już za pierwszym razem trafiliśmy do Psi Hotel Kanie! Doris otrzymała pokaźną dawkę ruchu i profesjonalną opiekę – a to wszystko udokumentowane na zdjęciach. Zadowolona i zmęczona psina to najlepsza rekomendacja. Na pewno skorzystamy po raz kolejny !'
+    },
   ]
+
+
+
+  
